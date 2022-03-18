6e443c469c0e72bfa78aff20a220fa8d12537bac
Lexarque <ignishalcyon1@gmail.com>
2022-03-18 20:17:32 +0700

    "Added Book Cover upload"

--- src/components/Book.vue
+++ src/components/Book.vue
@@ -32,6 +32,7 @@
                                 <td>{{ lb.author_book }}</td>
                                 <td>{{ lb.desc_book }}</td>
                                 <td>
+                                    <button class="btn btn-secondary" @click="Edit(lb)" data-bs-toggle="modal" data-bs-target="#bookcover_modal"><i class="fas fa-solid fa-image"></i></button>
                                     <button class="btn btn-info" @click="Edit(lb)" data-bs-toggle="modal" data-bs-target="#book_modal"><i class="fas fa-pencil-alt fa-fw"></i></button>
                                     <button class="btn btn-danger" @click="Delete(lb.id_book)"><i class="fas fa-trash-alt fa-fw"></i></button>
                                 </td>
@@ -69,12 +70,35 @@
                     </div>
                     <div class="modal-footer">
                         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
-                        <button type="button" class="btn btn-primary" @click="Save()">Submit</button>
+                        <button type="button" class="btn btn-primary" data-bs-dismiss="modal" @click="Save()">Submit</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="modal fade" id="bookcover_modal" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+            <div class="modal-dialog modal-lg">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="staticBackdropLabel">Book Cover</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+
+                        <div class="mb-3">
+                            <label for="book_cover" class="form-label">Book Cover</label>
+                            <input type="file" class="form-control" id="book_cover" @change="UploadCover($event)">
+                        </div>
+
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+                        <button type="button" class="btn btn-primary" @click="Upload(id_book)" data-bs-dismiss="modal">Submit</button>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+    
 </template>
 <script>
 module.exports = {
@@ -85,6 +109,7 @@ module.exports = {
             name_book: "",
             author_book: "",
             desc_book: "",
+            book_cover: "",
             action: "",
             list_book: [],
         }
@@ -101,6 +126,10 @@ module.exports = {
             })
         },
         Add: function() {
+            this.id_book= '' ,
+            this.name_book= '' ,
+            this.author_book= '' ,
+            this.desc_book= '' ,
             this.action = "insert"
         },
         //lb as in abbr of list book
@@ -126,13 +155,13 @@ module.exports = {
             if(this.action === 'insert'){ //POST
                 axios.post(api_url + '/add_book', form, token)
                 .then( res => {
-                    alert(res.data.message)
+                    Swal.fire(res.data.message, 'Data Added', 'success');
                 })
             } else 
             { //PUT
-              axios.put(api_url + '/book/' + this.id_book, token)
+              axios.put(api_url + '/book/' + this.id_book, form, token)
                 .then( res => {
-                    alert(res.data.message)
+                    Swal.fire(res.data.message, 'Data Updated', 'success');
             })}
 
             this.getData();
@@ -142,17 +171,51 @@ module.exports = {
             let token = {
                 headers : { "Authorization" : "Bearer " + this.$cookies.get("Authorization")}
             }
-            if(confirm("Do you want to delete this data?")){
-                
-                axios.delete(api_url + '/book/' + id_book, token)
-                .then( response => {
-                    alert(response.data.message)
-                    this.getData()
-                })
-            } else {
-                alert("Delete Cancelled");
+                Swal.fire({ 
+                        title: 'Are you sure?',
+                        text: "You won't be able to revert this!",
+                        icon: 'warning',
+                        showCancelButton: true,
+                        confirmButtonColor: '#3085d6',
+                        cancelButtonColor: '#d33',
+                        confirmButtonText: 'Yes, delete it!'
+                        }).then((result) => {
+                    if (result.isConfirmed) {
+                        axios.delete(api_url + '/book/' + id_book, token)
+                        .then( res => {
+                                Swal.fire(
+                                    'Deleted!',
+                                    res.data.message,
+                                    'success'
+                        )})
+                        this.getData()
+                    }else{
+                        Swal.fire(
+                            'Cancelled',
+                            'The process has been cancelled',
+                            'error'
+                        )
+                        this.getData()
+                    }
+                })    
+        },
+        UploadCover: function(e){
+            this.book_cover=e.target.files[0]
+        },
+        Upload: function(id_book){
+             let token ={
+                headers : {"Authorization" : "Bearer " + this.$cookies.get("Authorization"),'Content-Type':'multipart/form-data'}
             }
-        }
+
+            let form = new FormData()
+            form.append('book_cover', this.book_cover)
+
+            axios.post(api_url + '/book/upload_cover/'+ id_book, form, token)
+                .then(response => {
+                    alert(response.data.message)
+                })
+                this.getData()
+        },
     },
     
     mounted() {
